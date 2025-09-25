5258022245fd990f0f2e6645b6198e7009ad670e
imsuck <imsuck12@gmail.com>
2025-09-25 07:09:27 +0700

    massive blunder

--- test/yosupo/tree/dynamic_tree_subtree_add_subtree_sum_ett.test.cpp
+++ test/yosupo/tree/dynamic_tree_subtree_add_subtree_sum_ett.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/dynamic_tree_vertex_add_subtree_sum"
+#define PROBLEM "https://judge.yosupo.jp/problem/dynamic_tree_subtree_add_subtree_sum"
 
 #include <bits/stdc++.h>
 using namespace std;
